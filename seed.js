30d866d6d377b78cc3622f84e2bc70880f79e6b2
Brenni1 <phillip.brenndoerfer@gmail.com>
2024-04-11 11:02:55 +0200

    minor fixes

--- seed.js
+++ seed.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 const cardArray = require("../../oracle-cards-20240402090156.json");
 const CardModel = require("./models/Card.model.js");
-const MONGO_URI =
-  "mongodb+srv://mtg-deckbu-main-db-0039c1eed04:zz5brQ1UDrzgDUsnKGGx9jvXyDUFC9@prod-us-central1-2.ih9la.mongodb.net/mtg-deckbu-main-db-0039c1eed04";
 
+//define MONGO_URI in .env
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
